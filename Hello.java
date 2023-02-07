cff7c79497a032b7997d36b82585939734ae3fab
unknown <karne1cb@cmich.edu>
2023-02-06 20:25:13 -0500

    Initial commit :)

---
+++ Hello.java
@@ -0,0 +1,13 @@
+/**
+*
+* This is an example java class. Feel free to add more here.
+*
+*/
+
+public Class Hello(){
+	
+	public static void main(String[] args){
+		System.out.println("Hello ACM! That\'s not a Python program.");
+	}
+	
+}
---
+++ Hello.py
@@ -0,0 +1,3 @@
+# This is an example python file. Feel free to add more here :)
+
+print('Hello ACM! Now this, this is a Python program!')
